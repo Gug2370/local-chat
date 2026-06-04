a84c48a93ce57cede3008fc8853ee8f9ce088365
Gug2370 <dipengurunggugni2370@gmail.com>
2026-06-04 12:12:16 +0900

    First commit

---
+++ public/app.js
@@ -0,0 +1,39 @@
+const socket = io()
+
+const input = document.getElementById("input")
+
+const messages = document.getElementById("messages")
+
+function sendMessage(){
+
+    if(input.value.trim() === "") return
+
+    socket.emit("chat message", input.value)
+
+    input.value = ""
+
+}
+
+socket.on("chat message", (msg) => {
+
+    const div = document.createElement("div")
+
+    div.classList.add("message")
+
+    div.innerText = msg
+
+    messages.appendChild(div)
+
+    messages.scrollTop = messages.scrollHeight
+
+})
+
+input.addEventListener("keypress", (e) => {
+
+    if(e.key === "Enter"){
+
+        sendMessage()
+
+    }
+
+})
